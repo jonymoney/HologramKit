13db0f1d9e37f3028713dfb6a6140dab07977a85
Jonathan Velazquez <ijonathanvs@gmail.com>
2026-02-18 17:25:14 +0900

    Initial Commit

---
+++ Golden Ticket/Golden_TicketApp.swift
@@ -0,0 +1,17 @@
+//
+//  Golden_TicketApp.swift
+//  Golden Ticket
+//
+//  Created by Jonathan Velazquez on 2/18/26.
+//
+
+import SwiftUI
+
+@main
+struct Golden_TicketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
